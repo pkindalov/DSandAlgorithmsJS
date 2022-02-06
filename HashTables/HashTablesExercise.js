2a4d32c61805f84489e794153af8e4cc1fa60414
pkindalov <panayotkindalov@gmail.com>
2022-02-06 17:25:14 +0200

    Added private method getData to extract repeating code. It use for getting keys and values contained in the structure.

--- HashTables/HashTablesExercise.js
+++ HashTables/HashTablesExercise.js
@@ -12,6 +12,21 @@ class HashTable {
 		return hash;
 	}
 
+	_getData(index) {
+		let data = [];
+		this.data.forEach(shelf => {
+			//collision check to get collision data.
+			if (shelf && shelf.length > 1) {
+				shelf.forEach(collisionData => {
+					data.push(collisionData[index]);
+				});
+			} else {
+				data.push(shelf[0][index]);
+			}
+		});
+		return data;
+	}
+
 	set(key, value) {
 		const addressIndex = this._hash(key);
 		if (!this.data[addressIndex]) {
@@ -31,36 +46,13 @@ class HashTable {
 	}
 	keys() {
 		if (!this.data.length) return undefined;
-		let keys = [];
-		this.data.forEach(shelf => {
-			//collision check to get collision data.
-			if (shelf && shelf.length > 1) {
-				shelf.forEach(collisionData => {
-					keys.push(collisionData[0]);
-				});
-			} else {
-				keys.push(shelf[0][0]);
-			}
-		});
-		// for(let i = 0; i < this.data.length; i++) {
-		// 	if(this.data[i]) keys.push(this.data[i][0][0]);
-		// }
-		return keys;
+		const keysIndex = 0;
+		return this._getData(keysIndex);
 	}
 	values() {
 		if (!this.data.length) return undefined;
-		let values = [];
-		this.data.forEach(shelf => {
-			//collision check to get collision data.
-			if (shelf && shelf.length > 1) {
-				shelf.forEach(collisionData => {
-					values.push(collisionData[1]);
-				});
-			} else {
-				values.push(shelf[0][1]);
-			}
-		});
-		return values;
+		const keysIndex = 1;
+		return this._getData(keysIndex);
 	}
 }
 
