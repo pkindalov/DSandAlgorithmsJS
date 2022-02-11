cdfa192a0d208d5c61d5bb09029edafb8920b73c
pkindalov <panayotkindalov@gmail.com>
2022-02-11 11:39:40 +0200

    Added new class for creating nodes. Formatting the code

--- LinkedLists/DoublyLinkedList.js
+++ LinkedLists/DoublyLinkedList.js
@@ -1,143 +1,95 @@
-class DoublyLinkedList{
-    constructor(value){
-        this.head = {
-            value: value,
-            next: null,
-            prev: null
-        }
-        this.tail = this.head;
-        this.length = 1;
-    }
-    append(value) {
-        const newItem = {
-            value: value,
-            next: null,
-            prev: null
-        }
-        this.tail.next = newItem;
-        newItem.prev = this.tail;
-        this.tail = newItem;
-        this.length++;
-        return this;
-    }
+class Node {
+	constructor(value) {
+		this.value = value;
+		this.next = null;
+        this.prev = null;
+	}
+}
+class DoublyLinkedList {
+	constructor(value) {
+		this.head = new Node(value);
+		this.tail = this.head;
+		this.length = 1;
+	}
+	append(value) {
+		const newItem = new Node(value);
+		this.tail.next = newItem;
+		newItem.prev = this.tail;
+		this.tail = newItem;
+		this.length++;
+		return this;
+	}
 
-    prepend(value) {
-        const newItem = {
-            value: value,
-            next: null,
-            prev: null
-        }
+	prepend(value) {
+		const newItem = new Node(value);
+		newItem.next = this.head;
+		this.head.prev = newItem;
+		this.head = newItem;
+		this.length++;
+		return this;
+	}
+	print() {
+		let currentItem = this.head;
+		let items = [];
+		while (currentItem !== null) {
+			items.push(currentItem.value);
+			currentItem = currentItem.next;
+		}
+		return items;
+	}
+	size() {
+		return this.length;
+	}
+	insert(index, value) {
+		if (index === 0) {
+			this.prepend(value);
+			this.length++;
+			return this.print();
+		}
+		if (index >= this.length) return this.append(value);
+		const newItem = new Node(value);
+		const prevItem = this.traverseToIndex(index - 1);
+		const nextItem = prevItem.next;
+		prevItem.next = newItem;
+		newItem.prev = prevItem;
+		newItem.next = nextItem;
+		nextItem.prev = newItem;
+		this.length++;
+		return this.print();
+	}
 
-        newItem.next = this.head;
-        this.head.prev = newItem;
-        this.head = newItem;
-        this.length++;
-        return this;
-    }
-    print() {
-        let currentItem = this.head;
-        let items = [];
-        while(currentItem !== null) {
-            items.push(currentItem.value);
-            currentItem = currentItem.next;
-        }
-        return items;
-    }
-    size() {
-        return this.length;
-    }
-    insert(index, value) {
-        if(index === 0) {
-            this.prepend(value);
-            this.length++;
-            return this.print();
-        }
-        if (index >= this.length) return this.append(value);
-        const newItem = {
-            value: value,
-            next: null,
-            prev: null
-        }
-        const prevItem = this.traverseToIndex(index - 1);
-        const nextItem = prevItem.next;
-        prevItem.next = newItem;
-        newItem.prev = prevItem;
-        newItem.next = nextItem;
-        nextItem.prev = newItem;
-        // newItem.next = nextItem;
-        // newItem.prev = prevItem;
-        // nextItem.prev = newItem;
-        // prevItem.next = newItem;
-        this.length++;
-        return this.print();
-    }
+	remove(index) {
+		if (index === 0) {
+			const secondItem = this.head.next;
+			secondItem.prev = null;
+			this.head = secondItem;
+			this.length--;
+			return this.print();
+		}
+		let prevItem = this.traverseToIndex(index - 1);
+		let removingItem = prevItem.next;
+		let nextItem = removingItem.next;
 
-    remove(index) {
-        if(index === 0){
-            const secondItem = this.head.next;
-            secondItem.prev = null;
-            this.head = secondItem;
-            this.length--;
-            return this.print();
-        }
-        let prevItem = this.traverseToIndex(index-1);
-        let removingItem = prevItem.next;
-        let nextItem = removingItem.next;
+		prevItem.next = nextItem;
+		if (nextItem !== null) nextItem.prev = prevItem;
+		removingItem = null;
+		this.length--;
+		return this.print();
+	}
 
-        prevItem.next = nextItem;
-        if(nextItem !== null) nextItem.prev = prevItem;
-        removingItem = null;
-
-        //backup
-        // let prevItem = this.traverseToIndex(index-1);
-        // let currentItem = prevItem.next;
-        // prevItem.next = currentItem.next;
-        // currentItem = null;
-        this.length--;
-        return this.print();
-        
-
-    }
-
-    traverseToIndex(index) {
-        let currentItem = this.head;
-        let counter = 0;
-        while(counter !== index) {
-            currentItem = currentItem.next;
-            counter++;
-        }
-        return currentItem;
-    }
+	traverseToIndex(index) {
+		let currentItem = this.head;
+		let counter = 0;
+		while (counter !== index) {
+			currentItem = currentItem.next;
+			counter++;
+		}
+		return currentItem;
+	}
 }
 
 function getInstance(count) {
-    return new DoublyLinkedList(count);
+	return new DoublyLinkedList(count);
 }
 
 module.exports = getInstance;
-
-const list = new DoublyLinkedList(10);
-list.append(2);
-list.append(12);
-list.append(20);
-list.prepend(1);
-console.log(list.print());
-list.insert(2, 150);
-console.log(list.print());
-list.insert(1, 25);
-console.log(list.print());
-list.remove(1);
-console.log(list.print());
-list.remove(2);
-console.log(list.print());
-list.remove(list.size() - 1);
-console.log(list.print());
-
-
-//1, 10, 2, 12,20
-// // console.log(list.size());
-// list.insert(list.size(), 99);
-// console.log(list.print());
-// console.log(list.remove(1));
-// console.log(list.remove(2));
-// console.log(list.remove(0));
--- LinkedLists/LinkedList.js
+++ LinkedLists/LinkedList.js
@@ -1,110 +1,104 @@
+class Node {
+	constructor(value) {
+		this.value = value;
+		this.next = null;
+	}
+}
 class LinkedList {
-    constructor(item) {
-        this.head = {
-            value: item,
-            next: null
-        };
-        this.tail = this.head;
-        this.length = 1;
-    }
-    append(value) {
-        const newItem = {
-            value: value,
-            next: null
-        };
+	constructor(item) {
+		this.head = new Node(item);
+		this.tail = this.head;
+		this.length = 1;
+	}
+	append(value) {
+		const newItem = new Node(value);
 
-        this.tail.next = newItem;
-        this.tail = newItem;
-        this.length++;
-        return this;
-    }
-    prepend(value) {
-        const newItem = {
-            value: value,
-            next: null
-        };
+		this.tail.next = newItem;
+		this.tail = newItem;
+		this.length++;
+		return this;
+	}
+	prepend(value) {
+		const newItem = new Node(value);
 
-        newItem.next = this.head;
-        this.head = newItem;
-        this.length++;
-        return this;
-    }
-    print() {
-        const values = [];
-        let currentItem = this.head;
-        while (currentItem !== null) {
-            values.push(currentItem.value);
-            currentItem = currentItem.next;
-        }
-        return values;
-    }
-    insert(index, value) {
-        const newItem = {
-            value: value,
-            next: null
-        };
-        if (index === 0) {
-            this.prepend(value);
-            return this.print();
-        }
-        //my try
-        // if(index >= this.length) return this.append(value);
-        // let prevItem = this.head;
-        // let indexCount = 1;
-        // let nextItem = prevItem.next;
-        // while(nextItem){
-        //     if(index === indexCount) {
-        //         newItem.next = prevItem.next;
-        //         nextItem = newItem;
-        //         prevItem.next = newItem;
-        //         this.length++;
-        //         break;
-        //     }
-        //     prevItem = nextItem;
-        //     nextItem = nextItem.next;
-        //     indexCount++;
-        // }
+		newItem.next = this.head;
+		this.head = newItem;
+		this.length++;
+		return this;
+	}
+	print() {
+		const values = [];
+		let currentItem = this.head;
+		while (currentItem !== null) {
+			values.push(currentItem.value);
+			currentItem = currentItem.next;
+		}
+		return values;
+	}
+	insert(index, value) {
+		const newItem = new Node(value);
+		if (index === 0) {
+			this.prepend(value);
+			return this.print();
+		}
+		//my try
+		// if(index >= this.length) return this.append(value);
+		// let prevItem = this.head;
+		// let indexCount = 1;
+		// let nextItem = prevItem.next;
+		// while(nextItem){
+		//     if(index === indexCount) {
+		//         newItem.next = prevItem.next;
+		//         nextItem = newItem;
+		//         prevItem.next = newItem;
+		//         this.length++;
+		//         break;
+		//     }
+		//     prevItem = nextItem;
+		//     nextItem = nextItem.next;
+		//     indexCount++;
+		// }
 
-        //version from video tutorial
-        if (index >= this.length) return this.append(value);
-        const leader = this.traverseToIndex(index - 1);
-        const holdingPointer = leader.next;
-        leader.next = newItem;
-        newItem.next = holdingPointer;
-        this.length++;
-        return this.print();
-    }
+		//version from video tutorial
+		if (index >= this.length) return this.append(value);
+		const leader = this.traverseToIndex(index - 1);
+		const holdingPointer = leader.next;
+		leader.next = newItem;
+		newItem.next = holdingPointer;
+		this.length++;
+		return this.print();
+	}
 
-    remove(index) {
-        if (index === 0) {
-            let secondItem = this.head.next;
-            this.head = secondItem;
-            this.length--;
-            return this.print();
-        }
-        const leader = this.traverseToIndex(index - 1);
-        let holdingPointer = leader.next;
-        leader.next = holdingPointer.next;
-        holdingPointer = null;
-        this.length--;
-        return this.print();
-    }
-    size() {
-        return this.length;
-    }
-    traverseToIndex(index) {
-        let counter = 0;
-        let currentItem = this.head;
-        while (counter !== index) {
-            currentItem = currentItem.next;
-            counter++;
-        }
-        return currentItem;
-    }
+	remove(index) {
+		if (index === 0) {
+			let secondItem = this.head.next;
+			this.head = secondItem;
+			this.length--;
+			return this.print();
+		}
+		const leader = this.traverseToIndex(index - 1);
+		let holdingPointer = leader.next;
+		leader.next = holdingPointer.next;
+		holdingPointer = null;
+		this.length--;
+		return this.print();
+	}
+	size() {
+		return this.length;
+	}
+	traverseToIndex(index) {
+		let counter = 0;
+		let currentItem = this.head;
+		while (counter !== index) {
+			currentItem = currentItem.next;
+			counter++;
+		}
+		return currentItem;
+	}
 }
 
 function getInstance(count) {
-    return new LinkedList(count);
+	return new LinkedList(count);
 }
 
 module.exports = getInstance;
